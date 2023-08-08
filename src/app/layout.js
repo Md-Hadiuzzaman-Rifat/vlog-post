d021251fc710b9619ad3efe73df3d15b1e07b4af
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-08-08 16:03:47 +0600

    will work on curson

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,20 +3,22 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.scss";
 import AnimatedCursor from "react-animated-cursor";
 import { SessionProvider } from "next-auth/react";
+import ScrollComponent from "@/components/ScrollComponent/ScrollComponent";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="max-w-[1400px] center scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-black">
         <SessionProvider>
-          <AnimatedCursor
+          {/* <AnimatedCursor
             innerSize={10}
             outerSize={10}
             color="255, 255 ,255"
             outerAlpha={0.4}
             innerScale={0.7}
             outerScale={4}
-          />
+          /> */}
+          <ScrollComponent></ScrollComponent>
           <Navbar></Navbar>
           {children}
         </SessionProvider>
